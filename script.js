a7c001a940d60f6438f6a0b586ef06283a4b988a
SurenMangasaryan <suromangasaryan@gmail.com>
2022-11-28 15:01:55 +0400

    registration second-load

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 let inputs = document.querySelectorAll(".input");
 let btn = document.querySelector("button");
 
+let nameVal;
+let surenameVal;
+let passwordVal;
+
 for (let i = 0; i < inputs.length; i++) {
+
     let errors = document.createElement("p")
     errors.setAttribute("class", "errors")
     inputs[i].before(errors);
-}
 
-let div = document.createElement('div');
-div.setAttribute("class", "my-div");
-btn.after(div);
-
-btn.addEventListener("click", function(){
-
-    div.innerText = "";
-
-    for (let i = 0; i < inputs.length; i++) {
-        
+    inputs[i].addEventListener("input", function(){
         if (inputs[i].name == "name") {
             if (inputs[i].value.length > 6) {
-                div.innerHTML += `<p>${inputs[i].value}</p>`;
+                nameVal = inputs[i].value;
                 inputs[i].previousElementSibling.innerText = "";
             }
             else{
@@ -29,23 +24,38 @@ btn.addEventListener("click", function(){
 
         if (inputs[i].name == "surename") {
             if (inputs[i].value.length > 10) {
-                div.innerHTML += `<p>${inputs[i].value}</p>`;
+                surenameVal = inputs[i].value;
                 inputs[i].previousElementSibling.innerText = "";
             }
             else{
                 inputs[i].previousElementSibling.innerText = "The surename length must be greater than 10";
             }
         }
-  
+
         if (inputs[i].name == "password") {
             if (inputs[i].value.length > 8) {
-                div.innerHTML += `<p>${inputs[i].value}</p>`;
+                passwordVal = inputs[i].value;
                 inputs[i].previousElementSibling.innerText = "";
             }
             else{
                 inputs[i].previousElementSibling.innerText = "The password length must be greater than 8";
             }
         }
+    })
+}
+
+let div = document.createElement('div');
+div.setAttribute("class", "my-div");
+
+btn.addEventListener("click", function(){
+    if (nameVal.length !== 0 && surenameVal.length !== 0 && passwordVal.length !== 0) {
+        div.innerHTML = "";
+        
+        let myImg = document.createElement("img");
+        myImg.setAttribute("src", "https://termosfera.su/wp-content/uploads/2022/04/2816616767_vubrbej.jpg")
+        myImg.style.width = "150px";
+
+        div.innerHTML += `name: ${nameVal} <br> surename: ${surenameVal} <br> password: ${passwordVal} <br> profile picture <br> ${myImg}`;
+        btn.after(div);
     }
-    btn.after(div);
 })
